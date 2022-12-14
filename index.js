a57e10317193dc18cc226b3166a56b2f02a6c8ae
Valentyna Hnatyuk <hnatyukvalentyna01@gmail.com>
2022-12-14 15:24:00 +0100

    proxy

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ class State {
     constructor(initialState, callback) {
         this.initialState = { value: initialState };
         this.callback = typeof callback === 'function' ? callback : () => { };
+
+        const handler = {
+            get: (target, prop) => {
+                if(typeof target[prop] === "object") {
+                    return new Proxy(target[prop],handler)
+                }
+                return target[prop] // memory.state["counter"]
+            },
+            set: (target, prop, value) => {
+                target[prop] = value;
+                this.callback(this.state)
+                return true;
+            }
+        }
+
+        this.state = new Proxy(this.initialState.value, handler)
     }
 
-    get state() {
+   /* get state() {
         return this.initialState.value;
     }
 
@@ -15,13 +31,18 @@ class State {
         }
 
         this.callback(this.initialState.value);
-    }
+    } */
 }
 
-const memory = new State({counter: 0}, (newState) => {
+const memory = new State({
+    counter: 0, 
+    user: {id: 1}
+}, (newState) => {
     console.log(newState);
 } ); 
 
 // memory.state = {counter: memory.state.counter + 1};
 
-memory.state.counter = memory.state.counter + 1;
+ memory.state.counter = memory.state.counter + 1;
+
+
